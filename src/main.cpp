6b2f99b890d083aa7d43122775157537a4079440
kacpikoks11 <kacperugorny02@gmail.com>
2023-10-14 15:01:56 +0200

    7seg display

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,30 @@
 #define START_READ 1 // START BUTTON
 #define OUT1 3 // step
 #define OUT2 21 // impulse
+// malfunction ports 34 35 36 39
+#define LED1 16 // dont work
+#define LED2 32
+#define LED3 33
+#define LED4 27
 
-#define LED1 19
-#define LED2 18
-#define LED3 5
-#define LED4 17
-int LED[4] = {LED1, LED2, LED3, LED4};
-uint8_t seconds = 0;
+#define SEGA 26
+#define SEGB 14 // dont work
+#define SEGC 17 // dont work
+#define SEGD 18 
+#define SEGE 19
+#define SEGF 25
+#define SEGG 5 // dont work
+#define DOT 4
+
+int LED_DISPLAY[4] = {LED2, LED3, LED4, LED1};
+int SEG[7] = {SEGA, SEGB, SEGC, SEGD, SEGE, SEGF, SEGG};
+int nums[10][7] = {{SEGA, SEGB, SEGC, SEGD, SEGE, SEGF, SEGF}, {SEGB, SEGC, SEGB, SEGC, SEGB, SEGC, SEGB},
+                   {SEGA, SEGB, SEGG, SEGE, SEGD, SEGD, SEGD}, {SEGA,SEGB,SEGG,SEGC,SEGD,SEGC,SEGD},
+                   {SEGF, SEGB, SEGG, SEGC,SEGB, SEGG, SEGC}, {SEGA,SEGF,SEGG,SEGC,SEGD,SEGC,SEGD},
+                   {SEGA,SEGF,SEGG,SEGC,SEGD,SEGE,SEGE}, {SEGA,SEGB,SEGC,SEGA,SEGB,SEGC,SEGA},
+                   {SEGA, SEGB, SEGC, SEGD, SEGE, SEGF, SEGG}, {SEGA,SEGB,SEGG,SEGF,SEGC,SEGD,SEGD}
+                    };
+int seconds = 0;
 uint8_t history_up = 0;
 uint8_t history_down = 0;
 
@@ -33,7 +50,15 @@ void setup() {
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
   pinMode(LED4, OUTPUT);
-  ShowNum();
+
+  for(int seg : SEG){
+    pinMode(seg, OUTPUT_OPEN_DRAIN);
+    digitalWrite(seg, HIGH);
+  }
+  pinMode(DOT, OUTPUT_OPEN_DRAIN);
+  digitalWrite(DOT, HIGH);
+  
+  
 }
 
 
@@ -42,10 +67,12 @@ void loop() {
   if(digitalRead(START_READ) == HIGH && seconds != 0){
       //step
     digitalWrite(OUT1, HIGH);
-    while(seconds > 0){
-      delay(1000);
-      seconds--;
-      Serial.println(seconds);
+    ulong now = millis();
+    while(seconds != 0){
+      if(millis() - now > 1000){
+        seconds--;
+        now = millis();
+      }
       ShowNum();
     }
     digitalWrite(OUT1, LOW);
@@ -69,30 +96,67 @@ void loop() {
 
 
   if(history_up == 0b11110000){
-    seconds++;
-    Serial.println(seconds);
-    ShowNum();
-    
+    if(seconds != 599)
+      seconds++;
   }
   if(history_down == 0b11110000){
     if(seconds != 0)
       seconds--;
-    Serial.println(seconds);
-    ShowNum();
   }
+  ShowNum();
   
   
 }
 
 void ShowNum(){
-  if(seconds > 15)
-  {
-    digitalWrite(LED[0], HIGH);
-    digitalWrite(LED[1], HIGH);
-    digitalWrite(LED[2], HIGH);
-    digitalWrite(LED[3], HIGH);
-    return;
+  int mins = seconds / 60;
+  int onlyseconds = seconds % 60;
+  if(mins != 0){
+    digitalWrite(LED_DISPLAY[2], HIGH);
+    for(int seg : nums[mins])
+    {
+      digitalWrite(seg, LOW);
+    }
+    digitalWrite(DOT, LOW);
+    delay(1);
+    digitalWrite(DOT, HIGH);
+    for(int seg : SEG)
+    {
+      digitalWrite(seg, HIGH);
+    }
+    digitalWrite(LED_DISPLAY[2], LOW);
   }
-  for(int i = 0; i < 4; i++)
-    digitalWrite(LED[i], (seconds >> i & 0b0001));
+
+
+  digitalWrite(LED_DISPLAY[1], HIGH);
+  for(int seg : nums[onlyseconds/10])
+  {
+    digitalWrite(seg, LOW);
+  }
+  delay(1);
+  for(int seg : SEG)
+  {
+    digitalWrite(seg, HIGH);
+  }
+  digitalWrite(LED_DISPLAY[1], LOW);
+  
+
+
+  digitalWrite(LED_DISPLAY[0], HIGH);
+  for(int seg : nums[onlyseconds%10])
+  {
+    digitalWrite(seg, LOW);
+  }
+  delay(1);
+  for(int seg : SEG)
+  {
+    digitalWrite(seg, HIGH);
+  }
+  
+  digitalWrite(LED_DISPLAY[0], LOW);
+
+
+
+
+
 } 
